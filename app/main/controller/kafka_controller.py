350d9d79abbff3f48652d050f59ecc12fa2e0f8e
杨林强 <yanglinqiang@rd.tuan800.com>
2016-03-16 18:38:26 +0800

    no message

--- app/main/controller/kafka_controller.py
+++ app/main/controller/kafka_controller.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 
 from app.main.controller import login_required
+from app.main.model.cluster import Cluster
 from app.main.model.json_result import JsonResult
-from app.main.model.user import User
 from flask import render_template, Blueprint
 
 kafka_blueprint = Blueprint('kafka_blueprint', __name__)
@@ -17,7 +17,5 @@ def index():
 @kafka_blueprint.route('/cluster/simplelist', methods=['GET', 'POST'])
 @login_required
 def query_simple():
-    query = User.query.order_by(User.name.asc())
-    total = query.count()
-    rows = query.all()
-    return str(JsonResult(total, rows))
+    query = Cluster.query.order_by(Cluster.name.asc())
+    return str(JsonResult(query.count(), query.all()))
